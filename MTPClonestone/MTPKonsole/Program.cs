0bc8b42e3ba5e56d0ed64da0896bf12884a60a6c
it21-lp <leonhard.pfeiffer@gmail.com>
2016-08-23 11:36:18 +0200

    Kommentar erneut geändert

--- MTPClonestone/MTPKonsole/Program.cs
+++ MTPClonestone/MTPKonsole/Program.cs
@@ -11,7 +11,7 @@ namespace MTPKonsole
         static void Main(string[] args)
         {
             //Hallo
-            //Leonhard schreibt :)
+            //Pfeiffer schreibt :)
         }
     }
 }
