eec548134d2580ba4ab1bf88a2a6647d54c57649
chriskbbrz <christian.kloiber1@qualifizierung.or.at>
2016-09-12 12:16:54 +0200

    News Scene mit Elementen

---
+++ MTPClonestone/Clonestone/Assets/Scripts/9_News/LoadNews.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNews : MonoBehaviour {
+
+    public Vector2 scrollPosition = Vector2.zero;
+    public string innerText = "hi";
+
+    void OnGUI()
+    {
+        //GUI.BeginScrollView(new Rect(0, 0, 200, 200), scrollPosition, new Rect(0, 0, 190, 400));
+
+        //innerText = GUI.TextArea(new Rect(0, 0, 200, 300), innerText);
+
+        //GUI.EndScrollView();
+    }
+
+	// Use this for initialization
+	void Start () {
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
