0a4a9899eb0f66da04eeb1e99d01bf1c73e7d538
andiberndl <andi.berndl@gmail.com>
2016-09-19 09:50:09 +0200

    TestOrdnerPlayerPrefs

---
+++ MTPClonestone/OptionsStayLogedIn/ClonestoneTest/Assets/Scripts/Save.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class Save : MonoBehaviour {
+
+
+        public InputField username;
+        public InputField password;
+    
+    public void OnClick()
+    {
+        Debug.Log(username.text + ", " + password.text);
+        //StreamWriter swr; 
+        //TextWriter wr;
+        using (System.IO.StreamWriter writer = new StreamWriter("newfile.txt"))
+        {
+            writer.WriteLine(username.text + " " + password.text);
+        }
+
+    }
+
+}
