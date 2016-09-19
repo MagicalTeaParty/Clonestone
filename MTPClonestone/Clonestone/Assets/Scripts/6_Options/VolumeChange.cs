8f414f2ada50d6ed10853e925cc8b9d1846781ba
andiberndl <andi.berndl@gmail.com>
2016-09-19 10:15:14 +0200

    OptionsScene

---
+++ MTPClonestone/Clonestone/Assets/Scripts/6_Options/VolumeChange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VolumeChange : MonoBehaviour {
+
+    public Slider soundSlider;
+    public AudioSource volumeAudio;
+       
+
+	public void OnValueChange()
+    {
+        volumeAudio.volume = soundSlider.value;
+
+        Debug.Log(soundSlider.value);
+    }
+
+
+}
