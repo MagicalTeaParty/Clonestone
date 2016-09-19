359ee3f63f635dd1e5f06442193cfd1f9c4c44cf
MartinHengsberger <martin.hengsberger@gmail.com>
2016-09-19 10:49:47 +0200

    Login Scene erstellt, Datenabgleich mit MVC Projekt
    AdministrationController im MVC Projekt erstellt.

---
+++ MTPClonestone/Clonestone/Assets/Scripts/4_Login/Login.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+    public InputField email;
+    public InputField password;
+
+
+
+    public void onClick()
+    {
+        StartCoroutine("SendLoginInformation");
+    }
+
+    /// <summary>
+    /// Methode um die Eingabe des Users zu übermitteln
+    /// </summary>
+    /// <returns>string (www.text)</returns>
+    public IEnumerator SendLoginInformation()
+    {
+        string email, pass;
+
+        email = this.email.text;
+        pass = this.password.text;
+
+        //  TEST TEST TEST
+        Debug.Log("Email: " + email + "Passw: " + pass);
+
+        //WICHTIG! Controller wird mit dem Controllernamen ohne *Controller angesprochen! 
+        string saveUrl = "http://localhost:53861/Administration/Save";
+        WWWForm form = new WWWForm();
+        //WICHTIG! Formfelder müssen ident zu den Übergabewerten der Save() Methode sein!
+        form.AddField("email", email);
+        form.AddField("password", pass);
+        WWW www = new WWW(saveUrl, form);
+
+        yield return www;
+
+        //  TEST TEST TEST 
+        Debug.Log("Data: " + www.text);
+        Debug.Log("Error: " + www.error);
+    }
+
+
+}
---
+++ MTPClonestone/ClonestoneMVC/Controllers/AdministrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClonestoneMVC.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        [HttpPost]
+        public string Save(string email, string password)
+        {
+
+            ///TODO Vergleich email pw mit DB
+
+            //Ergebnis zurückschicken
+            return "OK";
+        }
+    }
+}
