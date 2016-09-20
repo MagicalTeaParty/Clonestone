b6ccd1ef836d90e7f0dfb9e5c68e8a9fc35e16ae
christian k <christian.kloiber1@qualifizierung.or.at>
2016-09-20 16:51:37 +0200

    Network Prefab Test2
    Neues Unityprojekt erstellt, Unity Git Einstellungen vorgenommen,
    Assets kopiert -> alles funktioniert. Es geht aber auch im ALTEN Projekt
    -> Es hängt also von der Unity Version ab!

---
+++ MTPClonestone/ClonestoneMultiplayerNetworkingV2/Assets/scripts/cardDrag.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+class cardDrag : NetworkBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+}
---
+++ MTPClonestone/ClonestoneMultiplayerNetworkingV2/Assets/scripts/cardPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Networking;
+
+class cardPlayer : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        transform.RotateAround(transform.position, transform.up, 180f);
+    }
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
