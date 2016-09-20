b1c7df90456771d0bd7833a66782843c05b8b35a
christian k <christian.kloiber1@qualifizierung.or.at>
2016-09-20 16:17:06 +0200

    neu erstellte szenen und assets

---
+++ MTPClonestone/ClonestoneUnity/Assets/Scripts/4_Login/Load.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class Load : MonoBehaviour {
+
+    public InputField email;
+    public InputField password;
+
+    public void Start()
+    {
+        
+
+        using (TextReader reader = File.OpenText("newfile.txt"))
+        {
+            string meinString = reader.ReadLine();
+            string[] meineStrings = meinString.Split(new char[] { ' ' });
+            email.text = meineStrings[0];
+            password.text = meineStrings[1];
+        }
+    }
+}
