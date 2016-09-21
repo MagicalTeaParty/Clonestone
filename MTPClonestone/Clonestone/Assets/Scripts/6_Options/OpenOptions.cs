f6a22dd5323999408a053bc8978b7a9c49084f4d
andiberndl <andi.berndl@gmail.com>
2016-09-21 14:07:47 +0200

    BackToPreviousScene Script hinzugefügt

---
+++ MTPClonestone/Clonestone/Assets/Scripts/6_Options/OpenOptions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OpenOptions : MonoBehaviour {
+
+	public void OptionsOpen()
+    {
+        PlayerPrefs.SetInt("previousLevel", Application.loadedLevel);
+        SceneManager.LoadScene(5);
+    }
+}
---
+++ MTPClonestone/TestProjektAndiManuel/ClonestoneTest/Assets/Scripts/6_Options/BackToPreviousScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackToPreviousScene : MonoBehaviour {
+
+	public void Back()
+    {
+        int previousLevel = PlayerPrefs.GetInt("previousLevel");
+        SceneManager.LoadScene(previousLevel);
+
+    }
+}
