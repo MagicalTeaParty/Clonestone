82ca2695d9993b210a0988ec55465df8706664c5
in21-lp <leonhard.pfeiffer@gmail.com>
2016-09-22 14:37:25 +0200

    Karte skalieren bei MouseEnter

--- MTPClonestone/ClonestoneMultiplayerNetworking/Assets/scripts/cardDrag.cs
+++ MTPClonestone/ClonestoneMultiplayerNetworking/Assets/scripts/cardDrag.cs
@@ -5,27 +5,55 @@ class cardDrag : NetworkBehaviour
 {
     private Vector3 screenPoint;
     private Vector3 offset;
+    private Vector3 originalScale;
 
+    //Scale the card up when the mouse hovers over it
+    void OnMouseEnter()
+    {
+        if (isLocalPlayer)
+        {
+            //Save object's current scale
+            originalScale = transform.localScale;
+
+            //Scale up until vector3.x reaches 0.8
+            while (transform.localScale.x < 0.8)
+            {
+                transform.localScale *= 1.01f;
+            } 
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (isLocalPlayer)
+        {
+            //Restore object's original scale
+            gameObject.transform.localScale = originalScale;
+        }
+    }
+
+    //Make the card draggable with the mouse
     void OnMouseDown()
     {
-        if (!isLocalPlayer)
+        if (isLocalPlayer)
         {
-            return;
+            //Save object's position
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            //Save difference between the mouse' and the object's positions
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
         }
-
-        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
     void OnMouseDrag()
     {
-        if (!isLocalPlayer)
+        if (isLocalPlayer)
         {
-            return;
+            //Save the current mouse position
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            //Convert the screen point to world point plus the difference between mouse and object
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            //Set the object's position to the new position
+            transform.position = curPosition;
         }
-
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
     }
 }
--- MTPClonestone/ClonestoneMultiplayerNetworking/Assets/scripts/cardPlayer.cs
+++ MTPClonestone/ClonestoneMultiplayerNetworking/Assets/scripts/cardPlayer.cs
@@ -2,14 +2,18 @@
 
 class cardPlayer : NetworkBehaviour
 {
+    //Should only happen for the local player
     public override void OnStartLocalPlayer()
     {
+        //Rotate the object up by 180°
         transform.RotateAround(transform.position, transform.up, 180f);
     }
     void Start()
     {
+        //The enemy player must not see the player's cards' front
         if (!isLocalPlayer)
         {
+            //Set the first child (= plane with active picture) inactive
             transform.GetChild(0).gameObject.SetActive(false);
         }
     }
