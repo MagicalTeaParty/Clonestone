9543f817845764b3dba466bd48efc4bf7df71ab6
christian k <christian.kloiber1@qualifizierung.or.at>
2016-09-22 09:32:23 +0200

    MusicPlayer

---
+++ MTPClonestone/Clonestone/Assets/Scripts/1_HagerGroup/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	public AudioClip menueClip;
+	public AudioClip gameClip;
+		
+	private AudioSource music;
+	
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+			music.clip = menueClip;
+			music.loop = true;
+			music.Play();
+		}
+	}
+	
+	void OnLevelWasLoaded(int level){
+		Debug.Log("MusicPlayer: loaded level "+level);
+		music.Stop ();
+		
+		if(level >= 0 && level <= 5){
+			music.clip = menueClip;
+		}
+		if(level == 6){
+			music.clip = gameClip;
+		}
+		
+		music.loop = true;
+		music.Play();
+	}
+}
