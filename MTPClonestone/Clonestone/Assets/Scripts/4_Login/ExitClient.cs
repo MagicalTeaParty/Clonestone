882cfe7a8cc71d0d2c783063ae245fe4b574d0e7
andiberndl <andi.berndl@gmail.com>
2016-09-26 11:13:54 +0200

    ExitClient Script hinzugefügt

---
+++ MTPClonestone/Clonestone/Assets/Scripts/4_Login/ExitClient.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitClient : MonoBehaviour {
+
+	public void onClick()
+    {
+        //Beendet den Client
+        Application.Quit();
+
+        //Dient nur zur Überprüfung da Application.Quit() nicht im "Testrun" funktioniert
+        Debug.Log("EXIT");
+    }
+}
---
+++ MTPClonestone/TestProjektAndiManuel/ClonestoneTest/Assets/Scripts/4_Login/ExitClient.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitClient : MonoBehaviour {
+
+	public void onClick()
+    {
+        Application.Quit();
+        Debug.Log("EXIT");
+    }
+}
