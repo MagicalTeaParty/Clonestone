10c701f509f9af542e8b85ec83f166e1f87d4824
MartinHengsberger <martin.hengsberger@gmail.com>
2016-10-17 15:28:14 +0200

    LoginController erweitert (Login MVC)

--- MTPClonestone/ClonestoneMVC/Controllers/LoginController.cs
+++ MTPClonestone/ClonestoneMVC/Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -23,9 +25,6 @@ namespace ClonestoneMVC.Controllers
             {
                 using(ClonestoneEntities cont = new ClonestoneEntities())
                 {
-                    //var res = (from l in cont.tbllogins
-                    //           where l.email == email && l.passcode == password
-                    //           select l).FirstOrDefault();
 
                     var gt = (from t in cont.tblpersons
                               join s in cont.tbllogins
@@ -36,7 +35,6 @@ namespace ClonestoneMVC.Controllers
                     if (gt != null)
                     {
                         return gt.Id + "|" + gt.Gamertag;
-                        //return Json(gt);
                     }
                 }
             }
@@ -54,6 +52,64 @@ namespace ClonestoneMVC.Controllers
             //Ergebnis zurückschicken
             //return "email or password WRONG!";
             return null;
+        }
+
+        // GET: PasswordForgotten
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string passcode)
+        {
+            string hashpass = getHashSha512(passcode);
+
+            try
+            {
+                using (ClonestoneEntities cont = new ClonestoneEntities())
+                {
+
+                    var gt = (from t in cont.tblpersons
+                              join s in cont.tbllogins
+                              on t.idperson equals s.idlogin
+                              where s.email == email && s.passcode == hashpass
+                              select new { Id = t.idperson, Gamertag = t.gamertag }).FirstOrDefault();
+
+                    if (gt != null)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO - Fehlerbehandlung
+                //Fehlerbehandlung
+            }
+            finally
+            {
+                //TODO - Fehlerbehandlung
+                //Aufräumarbeiten
+            }
+
+            //Ergebnis zurückschicken
+            //return "email or password WRONG!";
+            return null;
+
+        }
+
+        public static string getHashSha512(string pass)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(pass);
+            SHA512Managed hashstring = new SHA512Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
         }
     }
 }
