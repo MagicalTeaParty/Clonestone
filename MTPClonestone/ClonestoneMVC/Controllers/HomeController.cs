7856f118111085ab04a8cf66c8754e909ba0f1ed
MartinHengsberger <martin.hengsberger@gmail.com>
2016-10-20 13:07:35 +0200

    MVC Aufräumarbeiten

--- MTPClonestone/ClonestoneMVC/Controllers/HomeController.cs
+++ MTPClonestone/ClonestoneMVC/Controllers/HomeController.cs
@@ -11,36 +11,21 @@ namespace ClonestoneMVC.Controllers
     {
         public ActionResult Index()
         {
-            //Datenbankverbindung mittels EF
-            using(ClonestoneEntities cont = new ClonestoneEntities())
-            {
-                //Abfrage mittels LINQ
-                //var linqRes = (from c in cont.tblcards
-                //               where c.mana == 5
-                //               select c).ToList();
-
-                //Abfrage mittels Lambda
-                //List<tblcard> lambdaRes = cont.tblcards.Where(c=>c.mana==5).ToList();
-
-                //Abfrage mittels SP
-                //var spRes = cont.spGetMana5().ToList();
-            }
-
                 return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
