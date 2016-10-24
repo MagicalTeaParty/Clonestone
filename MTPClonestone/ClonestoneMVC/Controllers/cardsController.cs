154056a266598b1f602607620340566c582cfc30
andiberndl <andi.berndl@gmail.com>
2016-10-24 15:12:57 +0200

    Gallery

--- MTPClonestone/ClonestoneMVC/Controllers/cardsController.cs
+++ MTPClonestone/ClonestoneMVC/Controllers/cardsController.cs
@@ -15,11 +15,25 @@ namespace ClonestoneMVC.Controllers
         private ClonestoneEntities db = new ClonestoneEntities();
 
         // GET: cards
-        public ActionResult Index()
+        public ActionResult Index(int? start)
         {
+            if (start == null)
+            {
+                start = 0;
+            }
+          
+
+
             //.Include(t => t.tblability).Include(t => t.tblclass).Include(t => t.tbltype)
-            var tblcards = db.tblcards.Take(15);
-            return View(tblcards.ToList());
+            var cards = db.tblcards.OrderBy(tblcard => tblcard.idcard).Skip((int)start).Take(9);
+            var anz = db.tblcards.Count();
+
+            ViewBag.start = start;
+            ViewBag.steps = 9;
+            ViewBag.len = anz;
+            
+
+            return View(cards.ToList());
         }
 
     }
