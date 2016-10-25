f0fa56d1ebc165640b071bfad01d34835b51efb8
chriskbbrz <christian.kloiber1@qualifizierung.or.at>
2016-10-25 12:21:05 +0200

    Erweiterung als Child

--- Demos/3_CameraDemo/Assets/scripts/Networkdemo/playerNetworkController.cs
+++ Demos/3_CameraDemo/Assets/scripts/Networkdemo/playerNetworkController.cs
@@ -7,15 +7,24 @@ public class playerNetworkController : NetworkBehaviour
     public GameObject CardPrefab; //Platzhalter für das Karten-Prefab
     public Transform CardSpawnPosition; //Platzhalter für die Position des Spawnpunkts des aktuellen Spielers
 
+    [SyncVar]
+    float SyncYRot;
+
     void Start()
     {
         if(!isServer)
         {
-            this.transform.rotation = Quaternion.Euler(0, 180, 0);
+            CmdRotationSync(180);   
+        }
+        else
+        {
+            Camera.main.transform.rotation = Quaternion.Euler(90, 180, 0);
         }
     }
     	
 	void Update () {
+
+        RemoteRotationSync();
 
         if(!isLocalPlayer)
             return;
@@ -26,13 +35,33 @@ public class playerNetworkController : NetworkBehaviour
         }
     }
 
+    void RemoteRotationSync()
+    {
+        #region Allgemeine Änderung für ALLE Karten laut Sync-Variablen
+
+        this.transform.rotation = Quaternion.Euler(0, SyncYRot, 0);
+
+        #endregion
+    }
+
+    [Command]
+    void CmdRotationSync(float rotationX)
+    {
+        SyncYRot = rotationX;
+    }
+
     [Command]
     void CmdCardSpawnServer()
     {   
-        GameObject card = (GameObject)Instantiate(CardPrefab, CardSpawnPosition.position, CardSpawnPosition.rotation);
-                
-        NetworkServer.SpawnWithClientAuthority(card, this.connectionToClient);
+        //GameObject card = (GameObject)Instantiate(CardPrefab, CardSpawnPosition.position, CardSpawnPosition.rotation);
 
+        GameObject card = (GameObject)Instantiate(CardPrefab, CardSpawnPosition.transform);
+
+        card.transform.position = CardSpawnPosition.position;
+        card.transform.rotation = CardSpawnPosition.rotation;
+
+        NetworkServer.SpawnWithClientAuthority(card, this.connectionToClient);
+        
         //Hole das Skript der neuen Karte
         cardNetworkController cnc = card.GetComponent<cardNetworkController>();
 
