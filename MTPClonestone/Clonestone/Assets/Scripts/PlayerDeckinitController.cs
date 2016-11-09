05834dfd13b1359e7038d1d8156d7e6e9e2775c6
andiberndl <andi.berndl@gmail.com>
2016-11-09 13:36:20 +0100

    ...

---
+++ MTPClonestone/Clonestone/Assets/Scripts/PlayerDeckinitController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeckinitController : MonoBehaviour {
+
+    
+    public void onClick()
+    {
+        StartCoroutine("getDeck");
+    }
+
+
+    public IEnumerator getDeck()
+    {
+
+        string url = "http://mtp.a-k-t.at/clonestone/Deck/GetDeck";
+        WWWForm form = new WWWForm();
+        form.AddField("idDeck", 1);
+
+        WWW www = new WWW(url);
+        Debug.Log("hallo");
+
+        yield return www;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
