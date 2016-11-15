ea7949237301ff04d8068b1d12f045683745ef01
Manuel Sandholzer <htmanus@gmx.at>
2016-11-15 09:11:35 +0100

    JsonString in Klasse eingefügt

--- MTPClonestone/Clonestone/Assets/Scripts/PlayerDeckinitController.cs
+++ MTPClonestone/Clonestone/Assets/Scripts/PlayerDeckinitController.cs
@@ -1,5 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class JsonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+   
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
+
 
 public class PlayerDeckinitController : MonoBehaviour {
 
@@ -12,6 +31,16 @@ public class PlayerDeckinitController : MonoBehaviour {
 
     public IEnumerator getDeck()
     {
+        CardDataController.CardData[] data;
+
+        string jsonstring = "[{\"IdDeck\":1,\"DeckName\":\"default 1\",	\"IdClass\":7,	\"Class\":\"Paladin\",	\"IdType\":2,	\"TypeName\":\"Hero\",	\"IdCard\":705,	\"CardName\":\"Paladin\",	\"Mana\":0, \"Attack\":0,	\"Health\":30,		\"Flavour\":null, \"IdDeckCard\":31,	\"FileName\":\"705.png\",	\"zahl\":\"00000000-0000-0000-0000-000000000000\" }, {\"IdDeck\":1,\"DeckName\":\"default 1\",	\"IdClass\":7,	\"Class\":\"Paladin\",	\"IdType\":2,	\"TypeName\":\"Hero\",	\"IdCard\":705,	\"CardName\":\"Paladin\",	\"Mana\":0, \"Attack\":0,	\"Health\":30,		\"Flavour\":null, \"IdDeckCard\":31,	\"FileName\":\"705.png\",	\"zahl\":\"00000000-0000-0000-0000-000000000000\" }]";
+
+        //data = JsonUtility.FromJson<List<CardDataController.CardData>>(jsonstring);
+
+        data = JsonHelper.getJsonArray<CardDataController.CardData>(jsonstring);
+
+
+        //data = JsonUtility.FromJson<CardDataController.CardData>("{\"IdDeck\":1,\"DeckName\":\"default 1\",	\"IdClass\":7,	\"Class\":\"Paladin\",	\"IdType\":2,	\"TypeName\":\"Hero\",	\"IdCard\":705,	\"CardName\":\"Paladin\",	\"Mana\":0, \"Attack\":0,	\"Health\":30,		\"Flavour\":null, \"IdDeckCard\":31,	\"FileName\":\"705.png\",	\"zahl\":\"00000000-0000-0000-0000-000000000000\" }");
 
         string url = "http://localhost:53861/Deck/GetDeck";
         WWWForm form = new WWWForm();
@@ -22,6 +51,12 @@ public class PlayerDeckinitController : MonoBehaviour {
 
         yield return www;
 
+
+        //List<CardDataController.CardData> data = JsonUtility.FromJson<List<CardDataController.CardData>>(www.text);
+
+
+          
+        
     }
 
     // Update is called once per frame
