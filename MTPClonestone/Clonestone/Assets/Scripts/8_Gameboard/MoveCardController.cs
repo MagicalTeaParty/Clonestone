03399c1ce8a8221af34a6d0b45d983d11ebed2db
MartinHengsberger <martin.hengsberger@gmail.com>
2016-11-22 08:29:40 +0100

    CardMove, Card Prefab modifiziert

---
+++ MTPClonestone/Clonestone/Assets/Scripts/8_Gameboard/MoveCardController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MoveCardController : MonoBehaviour {
+
+
+    public GameObject dropZone;
+    public GameObject cardHand;
+    public GameObject deckposition;
+    public GameObject heroPosition;
+    public GameObject card;
+    public List<GameObject> cards;
+
+    /// <summary>
+    /// Bewegt Card Objekte zwischen "Parent-Elementen"
+    /// </summary>
+    public void MoveCard(GameObject card, GameObject placeToDrop)
+    {
+        if (placeToDrop == heroPosition)
+        {
+            card.GetComponent<LayoutElement>().enabled = false;
+        }
+
+        card.transform.parent = placeToDrop.transform;
+        card.SetActive(true);
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        MoveCard(card, heroPosition);
+
+	}
+	
+
+}
