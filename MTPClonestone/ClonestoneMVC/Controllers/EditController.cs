5fbc33204f54d80cdf44e0cf0d65429f87ab5ede
thomasfaul <thomas.faul@gmx.at>
2016-12-07 18:04:34 +0100

    Editcontrollers und views eingefügt Anpassung fehlt

---
+++ MTPClonestone/ClonestoneMVC/Controllers/EditController.cs
@@ -0,0 +1,39 @@
+﻿using ClonestoneMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClonestoneMVC.Controllers
+{
+    public class EditController : Controller
+    {
+
+        private ClonestoneEntities db = new ClonestoneEntities();
+
+        // GET: /Edit
+        public ActionResult Index()
+        {
+            //var news = (b => b.tblpersons);
+            return View(db.tbledits.ToList());
+        }
+
+        // GET: /Detail/Id
+
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var news = db.tbledits.Find(id);
+            if(news == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+    }
+}
